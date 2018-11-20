9d1b0466539705560c0d8f095845c424b56ecd6e
alex10151 <625695174@qq.com>
2018-11-20 10:37:46 +0800

    modified

--- scripts/covert2dcm.py
+++ scripts/covert2dcm.py
@@ -2,15 +2,24 @@ import os
 import re
 import copy
 
-path = "C:/Users/ALEX_HSW/source/repos/Project2/Project2/1/trans"
-#pat = re.compile(r"[\w.)(]+.IMA$")
-pat = re.compile(r"[\w.)(]+.tif$")
-dirs = os.listdir(path)
+dir_root = "C:/Users/ALEX_HSW/Desktop/CT去骨测试数据/data"
+# path = "C:/Users/ALEX_HSW/source/repos/Project2/Project2/1/trans"
+# pat = re.compile(r"[\w.)(]+.IMA$")
+pat = re.compile(r"[\w.)(]+.IMA$")
+
 if __name__ == '__main__':
-    for name in dirs:
-        if re.match(pat, name) is not None:
-            # print(name)
-            # index = name.split(".")
-            res = name.replace('tif', 'dcm')
-            print(res)
-            os.rename(path + "/" + name, path + "/" + res)
+    for name in os.listdir(dir_root):
+        name = dir_root + "/" + name + "/FDG/"
+        for name2 in os.listdir(name):
+            name2 = name + name2
+            for name3 in os.listdir(name2):
+                if re.match(pat, name3) is not None:
+                    # print(name)
+                    # index = name.split(".")
+                    res = name3.replace('IMA', 'dcm')
+                    print(res)
+                    os.rename(name2 + "/" + name3, name2 + "/" + res)
+                else:
+                    res = name3 + ".dcm"
+                    print(res)
+                    os.rename(name2 + "/" + name3, name2 + "/" + res)
